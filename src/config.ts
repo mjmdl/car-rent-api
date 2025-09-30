3371e2ddcee3fed31da15f0c8e320d40416ede3c
Matheus Maidel <matheus.maidel@gmail.com>
2025-09-30 18:31:05 -0300

    chore: config class validator/transformer

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,23 @@
-import { INestApplication } from "@nestjs/common";
+import { INestApplication, Type, ValidationPipe } from "@nestjs/common";
+import { useContainer } from "class-validator";
 
 export function configCors(app: INestApplication): void {
     app.enableCors({
         origin: '*',
         methods: ['DELETE', 'GET', 'PATCH', 'POST', 'PUT'],
     });
+}
+
+export function configValidator(app: INestApplication): void {
+    app.useGlobalPipes(new ValidationPipe({
+        always: true,
+        forbidNonWhitelisted: true,
+        forbidUnknownValues: true,
+        transform: true,
+        whitelist: true,
+    }));
+}
+
+export function configTransformer(app: INestApplication, module: Type): void {
+    useContainer(app.select(module), { fallbackOnErrors: true });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { configCors } from './config';
+import { configCors, configTransformer, configValidator } from './config';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
 
 	configCors(app);
+	configValidator(app);
+	configTransformer(app, AppModule);
 
 	await app.listen(3000, async () => {
 		const url = await app.getUrl();
